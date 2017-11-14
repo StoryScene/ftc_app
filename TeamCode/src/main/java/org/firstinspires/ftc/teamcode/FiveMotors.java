248cd42edbd3ba4344da5871cd74b88872081002
StoryScene <emmaqin0722@gmail.com>
2017-11-13 16:42:23 -0800

    Code for mechanum

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FiveMotors.java
@@ -0,0 +1,66 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+
+/**
+ * Created by Emma on 11/10/17.
+ */
+
+public class FiveMotors extends OpMode {
+
+    DcMotor motorLeft;
+    DcMotor motorRight;
+    DcMotor grabLeft;
+    DcMotor grabRight;
+    DcMotor arm;
+
+    @Override
+    public void init(){
+        motorLeft = hardwareMap.dcMotor.get("left");
+        motorRight = hardwareMap.dcMotor.get("right");
+        grabLeft = hardwareMap.dcMotor.get("grab1");
+        grabRight = hardwareMap.dcMotor.get("grab2");
+        arm = hardwareMap.dcMotor.get("arm");
+
+        motorLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+    }
+
+    @Override
+    public void loop() {
+
+        motorLeft.setPower(gamepad1.left_stick_y);
+        motorRight.setPower(gamepad1.right_stick_y);
+
+        if (gamepad1.x){
+            grabLeft.setPower(.5);
+            grabRight.setPower(-.5);
+        }
+        else if (gamepad1.y){
+            grabLeft.setPower(-.5);
+            grabRight.setPower(.5);
+        }
+        else{
+            grabLeft.setPower(0);
+            grabRight.setPower(0);
+        }
+        if (gamepad1.left_bumper){
+            arm.setPower(.5);
+        }
+        if (gamepad1.right_bumper){
+            arm.setPower(0);
+        }
+
+        telemetry.addData("right bumper",gamepad1.right_bumper);
+        telemetry.addData("left bumper",gamepad1.left_bumper);
+        telemetry.addData("a",gamepad1.a);
+        telemetry.addData("b",gamepad1.b);
+        telemetry.addData("x",gamepad1.x);
+        telemetry.addData("y",gamepad1.y);
+        telemetry.addData("right stick", gamepad1.right_stick_y);
+        telemetry.addData("left stick", gamepad1.left_stick_y);
+
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MechanumDrive.java
@@ -0,0 +1,83 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.Range;
+
+/**
+ * Created by Emma on 11/13/17.
+ */
+
+public class MechanumDrive extends OpMode {
+
+    DcMotor lf, rf, lb, rb;
+
+
+    @Override
+    public void init() {
+        mechanumInit();
+    }
+
+    @Override
+    public void loop() {
+        mechanumLoop();
+        telemetry.addData("Power of lf, rf, lb, rb:\n", lf.getPower() + "\n" + rf.getPower()
+                + "\n" + lb.getPower() + "\n" + rb.getPower());
+
+    }
+
+    private double maxPow(double x, double y, double z, double w) {
+        x = Math.abs(x);
+        y = Math.abs(y);
+        z = Math.abs(z);
+        w = Math.abs(w);
+        return Math.max(Math.max(x,y), Math.max(z,w));
+    }
+
+
+    private void mechanumInit() {
+        lf = hardwareMap.dcMotor.get("leftF");
+        rf = hardwareMap.dcMotor.get("rightF");
+        lb = hardwareMap.dcMotor.get("leftB");
+        rb = hardwareMap.dcMotor.get("rightB");
+    }
+
+
+    private void mechanumLoop() {
+        double x = Range.clip(gamepad1.left_stick_x, -1, 1);
+        double y = Range.clip(gamepad1.left_stick_y, -1, 1);
+        double rot = Range.clip(gamepad1.right_stick_x, -1, 1);
+
+        double r = Math.hypot(x, y);
+        double angle = 0.0;
+
+
+        if (r > 0.1){
+            angle = Math.atan2(y,x) - Math.PI / 4;
+        }
+
+        telemetry.addData("angle: ", angle);
+        telemetry.addData("radius: ", r);
+        telemetry.addData("rotate: ", rot);
+
+
+        double vlf = r * Math.cos(angle) + rot;
+        double vrf = r * Math.sin(angle) - rot;
+        double vlb = r * Math.sin(angle) + rot;
+        double vrb = r * Math.cos(angle) - rot;
+
+        double maxPower = maxPow(vlf, vrf, vlb, vrb);
+
+        vlf /= maxPower;
+        vrf /= maxPower;
+        vlb /= maxPower;
+        vrb /= maxPower;
+
+        lf.setPower(Range.clip(vlf, -1, 1));
+        rf.setPower(Range.clip(vrf, -1, 1));
+        lb.setPower(Range.clip(vlb, -1, 1));
+        rb.setPower(Range.clip(vrb, -1, 1));
+
+        telemetry.addData("maxPower: ", maxPower);
+    }
+}
