e5d4a6dab6d2e8ff0409c204bed5417aeb1d8e00
StoryScene <emmaqin0722@gmail.com>
2017-12-02 12:42:37 -0800

    Temp Auto

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Temp_Auto.java
@@ -0,0 +1,44 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import org.firstinspires.ftc.teamcode.modules.StateMachine;
+
+import java.util.Timer;
+
+/**
+ * Created by Emma on 12/2/17.
+ */
+
+public class Temp_Auto extends OpMode {
+
+    DcMotor lf, rf, lb, rb;
+    Wheels wheels;
+
+
+    @Override
+    public void init() {
+        lf = hardwareMap.dcMotor.get("leftF");
+        rf = hardwareMap.dcMotor.get("rightF");
+        lb = hardwareMap.dcMotor.get("leftB");
+        rb = hardwareMap.dcMotor.get("rightB");
+
+        wheels = new Wheels(lf, rf, lb, rb);
+
+        wheels.initialize();
+        resetStartTime();
+    }
+
+    @Override
+    public void loop() {
+        telemetry.addData("Wheels power: ", lf.getPower() + "\n" + rf.getPower() + "\n" + lb.getPower() + "\n" + rb.getPower());
+        if (getRuntime() < 2) {
+            wheels.move(0,1,0);
+        }
+        else {
+            wheels.move(0,0,0);
+        }
+
+    }
+}
