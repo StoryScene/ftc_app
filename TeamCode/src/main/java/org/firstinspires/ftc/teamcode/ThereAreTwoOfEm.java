aa9d07489628ce758a887153cb1550884d8a26fc
billcipher1344 <baconrulz101@gmail.com>
2018-01-13 13:29:47 -0800

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ThereAreTwoOfEm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ThereAreTwoOfEm.java
@@ -4,6 +4,8 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+
 /**
  * Created by Kit Caldwell on 1/9/2018.
  */
@@ -11,20 +13,18 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 @TeleOp
 public class ThereAreTwoOfEm extends OpMode{
 
-    DcMotor leftMotor;
-    DcMotor rightMotor;
+    Servo servo1;
+    Servo servo2;
 
     public void init(){
-        leftMotor = hardwareMap.dcMotor.get("leftMotor");
-        rightMotor = hardwareMap.dcMotor.get("rightMotor");
+        servo1 = hardwareMap.servo.get("one");
+        servo2 = hardwareMap.servo.get("two");
 
-        leftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
     }
 
     public void loop(){
 
-        leftMotor.setPower(gamepad1.left_stick_y);
-        rightMotor.setPower(gamepad1.right_stick_y);
-
+        servo1.setPosition(1);
+        servo2.setPosition(-1);
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/fourMotorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/fourMotorTest.java
@@ -21,50 +21,61 @@ public class fourMotorTest extends OpMode {
     DcMotor grabLeft;
     DcMotor grabRight;
 
-    Servo rotate1;
-    Servo arm;
+    CRServo rotate1;
+    //Servo arm;
 
     public void init(){
         motorLeft = hardwareMap.dcMotor.get("left");
         motorRight = hardwareMap.dcMotor.get("right");
         grabLeft = hardwareMap.dcMotor.get("grab1");
         grabRight = hardwareMap.dcMotor.get("grab2");
-        rotate1 = hardwareMap.servo.get("rotate1");
-        arm = hardwareMap.servo.get("arm");
+        rotate1 = hardwareMap.crservo.get("servo");
+        //arm = hardwareMap.servo.get("arm");
 
         motorLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        motorLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motorRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        motorRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        motorLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
     }
     public void loop(){
 
-        double power = gamepad1.left_stick_y;
+        double power = -.6*gamepad1.left_stick_y;
 
         motorLeft.setPower(power);
-        motorRight.setPower(gamepad1.right_stick_y);
+        motorRight.setPower(-.6* gamepad1.right_stick_y);
 
+        //intake
         if (gamepad1.x == true){
             grabLeft.setPower(.5);
             grabRight.setPower(-.5);
         }
+        //put out
         else if (gamepad1.y == true){
-            grabLeft.setPower(-.5);
-            grabRight.setPower(.5);
+            grabLeft.setPower(-.25);
+            grabRight.setPower(.25);
         }
         else{
             grabLeft.setPower(0);
             grabRight.setPower(0);
         }
         if (gamepad1.a == true) {
-            rotate1.setPosition(1);
+            rotate1.setPower(1) ;
         }
-        if (gamepad1.b == true) {
-            rotate1.setPosition(0);
+        else if (gamepad1.b == true) {
+            rotate1.setPower(-1);
         }
-        if (gamepad1.left_bumper == true){
-            arm.setPosition(1);
+        else{
+            rotate1.setPower(0);
         }
-        if (gamepad1.right_bumper == true){
-            arm.setPosition(0);
-        }
+        //if (gamepad1.left_bumper == true){
+            //arm.setPosition(1);
+        //}
+        //if (gamepad1.right_bumper == true){
+            //arm.setPosition(0);
+        // }
 
         telemetry.addData("right bumper",gamepad1.right_bumper);
         telemetry.addData("left bumper",gamepad1.left_bumper);
